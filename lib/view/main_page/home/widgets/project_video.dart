c538e4fdbe9af76d5c2f917ee172e6fde4a492ce
ajay-infozzle <ajay.m@infozzle.com>
2026-02-18 17:09:48 +0530

    verision 4.0.11+31

--- lib/view/main_page/home/widgets/project_video.dart
+++ lib/view/main_page/home/widgets/project_video.dart
@@ -1,11 +1,14 @@
 import 'dart:convert';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:http/http.dart' as http;
 import 'package:intl/intl.dart';
 import 'package:marathon/data/server/api/api_const.dart';
 import 'package:webview_flutter/webview_flutter.dart';
+
+import '../../../../data/tools/decoration/res_colors.dart';
 
 class ProjectVideoSection extends StatefulWidget {
   final String email;
@@ -72,6 +75,14 @@ class _ProjectVideoSectionState extends State<ProjectVideoSection> {
     );
 
     final data = jsonDecode(response.body);
+    if (kDebugMode) {
+      print("--<< Video API called: ${{
+        "project": widget.project,
+        "tower": widget.tower,
+        "unit": widget.unit,
+      }}");
+      print("--<< Video API Response: $data");
+    }
 
     /// Error Case
     if (data["error"] != null) {
@@ -126,7 +137,19 @@ class _ProjectVideoSectionState extends State<ProjectVideoSection> {
   /// PLAY VIDEO
   /// ---------------------------
   void _playVideo() {
-    if (isVideoLoaded || videoData == null) return;
+    if (videoData == null) return;
+    if(showVideo){
+      setState(() {
+        showVideo = false;
+      });
+      return ;
+    }
+    if(isVideoLoaded){
+      setState(() {
+        showVideo = true;
+      });
+      return ;
+    }
 
     final videoUrl = _buildVideoUrl();
     controller.loadRequest(Uri.parse(videoUrl));
@@ -161,21 +184,51 @@ class _ProjectVideoSectionState extends State<ProjectVideoSection> {
         /// Show only if API success
         if (showVideoButton)
           Padding(
-            padding:
-                const EdgeInsets.symmetric(horizontal: 16),
-            child: InkWell(
-              onTap: _playVideo,
-              child: const Text(
-                'View Your Apartment',
-                style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.w600,
-                  decoration: TextDecoration.underline,
-                  color: Colors.blue,
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child:TextButton(
+            onPressed: _playVideo,
+            style: ButtonStyle(
+              backgroundColor: WidgetStateProperty.all(ColorRes.mainButtonColor.withValues(alpha:0.1)),
+            ),
+            child: Row(
+              mainAxisAlignment: .center,
+              children: [
+                Icon(
+                  showVideo ? Icons.pause_circle_filled :Icons.play_circle_filled,
+                  color: ColorRes.mainButtonColor,
+                  size: 30,
                 ),
-              ),
+                const SizedBox(width: 4),
+                Text(
+                  'View Your Apartment',
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w600,
+                    color: ColorRes.mainButtonColor,
+                  ),
+                ),
+              ],
             ),
           ),
+          ),
+
+        // if (showVideoButton)
+        //   Padding(
+        //     padding:
+        //         const EdgeInsets.symmetric(horizontal: 16),
+        //     child: InkWell(
+        //       onTap: _playVideo,
+        //       child: const Text(
+        //         'View Your Apartment',
+        //         style: TextStyle(
+        //           fontSize: 16,
+        //           fontWeight: FontWeight.w600,
+        //           decoration: TextDecoration.underline,
+        //           color: Colors.blue,
+        //         ),
+        //       ),
+        //     ),
+        //   ),
 
         if (showVideo)
           const SizedBox(height: 12),
